c41835a54bd8f0ba50b3f9c2731eb213f458c264
kushkp <ankushp89@gmail.com>
2015-06-11 15:35:41 -0700

    Two player functionality. Ship placement still random.

--- w3/w3d3/battleship.rb
+++ w3/w3d3/battleship.rb
@@ -1,37 +1,51 @@
+#Goad: enable 2p game
+require 'byebug'
+require 'set'
+
 class Board
   attr_reader :grid, :ship_locations, :size
   attr_accessor :counts
 
-  STATUS = { :miss => "O", :hit => "X", :water => "~" }
+  STATUS = { :miss => "O", :hit => "X", :water => "~" , :ship => "~"}
 
   def initialize(size)
     @size = size
-    @ship_locations = []
-    @fire_locations = []
     @counts = {:ships => 5, :misses => 0}
-    @counts[:water] = size * size - @counts[:ships]
-    @grid = Array.new(size) {Array.new(size) { STATUS[:water] } }
+    @grid = Array.new(size) { Array.new(size) { :water } }
   end
 
-  def [](row,col)
+  def [](row, col)
     @grid[row][col]
   end
 
-  def []=(row, col, mark)
-    @grid[row][col] = mark
+  def []=(row, col, val)
+    @grid[row][col] = val
   end
 
   def display
-    @grid.each { |row| p row }
+    p "  #{(0...@size).to_a.join(" ")}"
+    @grid.each_with_index do |row, index|
+       p display_row(row, index)
+    end
+  end
+
+  def display_row(row, index)
+    chars = row.map { |el| STATUS[el] }.join(" ")
+    "#{index} #{chars}"
   end
 
   def populate_grid
-    until @ship_locations.count >= @counts[:ships]
+    until @grid.flatten.count(:ship) == 5
       row = Random.new.rand(@size)
       col = Random.new.rand(@size)
-      next if @ship_locations.include?([row,col])
-      @ship_locations << [row,col]
+      self[row, col] = :ship
     end
+  end
+
+  def print_counts
+    ships_remaining = @grid.flatten.count(:ship)
+    misses = @grid.flatten.count(:miss)
+    puts "Ships remaining: #{ships_remaining}, Misses: #{misses}"
   end
 
   def in_range?(row, col)
@@ -39,100 +53,159 @@ class Board
   end
 
   def mark_board(row, col)
-    if @fire_locations.include?([row, col])
-      puts "Already fired here!"
-    elsif @ship_locations.include?([row, col])
-      @grid[row][col] = STATUS[:hit]
-      @counts[:ships] -= 1
-      @ship_locations.delete([row, col])
-      @fire_locations << [row, col]
-    else
-      @grid[row][col] = STATUS[:miss]
+    case self[row, col]
+    when :hit, :miss
+      puts "You already fired here!"
+    when :water
+      self[row, col] = :miss
       @counts[:misses] += 1
-      @counts[:water] -= 1
-      @fire_locations << [row, col]
+    when :ship
+      self[row, col] = :hit
+      @counts[:ships] -= 1
     end
+  end
+
+  def ship_locations #only for debugging
+    ship_locs_arr = []
+    @grid.each_index do |row|
+      @grid[row].each_index do |col|
+        if self[row, col] == :ship
+          ship_locs_arr << [row, col]
+        end
+      end
+    end
+    # p ship_locs_arr #for debugging
   end
 end
 
 class Game
-
-  def initialize(board_size = 8)
-      @game_board = Board.new(board_size)
-      @player1 = HumanPlayer.new
+  def initialize(board_size = 4)
+      @player1 = HumanPlayer.new("PeeWee", 4)
+      @player2 = ComputerPlayer.new("Naval Commander", 4)
+      @this_player = @player2
   end
 
   def play
-    @game_board.populate_grid
-    welcome_msg
+    @player1.board.populate_grid
+    @player2.board.populate_grid
+    welcome_message
     until game_over?
-      play_turn
+      change_turn
+      play_turn(@this_player)
     end
     end_message
   end
 
-  def welcome_msg
+  def welcome_message
     puts "Welcome to BattleShip!"
     puts "Fire at location (x,y) by entering 'x,y'"
+    # display_board
+  end
+
+  def change_turn
+    @this_player = other_player
+  end
+
+  def other_player
+    @this_player == @player1 ? @player2 : @player1
+  end
+
+  def play_turn(player)
+    display_board
+    puts "#{player.name}'s Turn:'"
+    fire_at = player.get_input
+    until @this_player.board.in_range?(*fire_at) #until valid_move?
+      puts "Invalid location. Please enter location within (0-#{@this_player.board.size-1})x(0-#{@this_player.board.size-1})."
+      fire_at = player.get_input
+    end
+    attack(fire_at)
+    display_board
     display_status
   end
 
-  def play_turn
-    fire_at = @player1.get_user_input
-    until @game_board.in_range?(*fire_at)
-      puts "Invalid location. Please enter location within (0-#{@game_board.size-1})x(0-#{@game_board.size-1})."
-      fire_at = @player1.get_user_input
-    end
-    attack(fire_at)
-    display_status
+  def valid_move?
+
   end
 
   def attack(pos)
-    @game_board.mark_board(*pos)
+    @this_player.board.mark_board(*pos)
+  end
+
+  def display_board
+    @this_player.board.display
   end
 
   def display_status
-    @game_board.display
-    p @game_board.counts
-    p @game_board.ship_locations
+    @this_player.board.print_counts
+    @this_player.board.ship_locations #for debugging
+    puts "\n"
   end
 
   def game_over?
-    if @game_board.counts[:ships] == 0 || @game_board.counts[:water] == 0
+    @this_player.board.counts[:ships] == 0 ? true : false
+  end
+
+  def end_message
+    change_turn unless @this_player.board.counts[:ships] == 0
+    puts "Congrats, #{@this_player.name} wins!"
+  end
+end
+
+class HumanPlayer
+  attr_reader :name
+  attr_accessor :board
+
+  def initialize(name, board_size)
+    @name = name
+    @board = Board.new(board_size)
+  end
+
+  def get_input
+    puts "Where would you like to fire?"
+    user_input = gets.chomp.split(",")
+    until valid_input?(user_input)
+      puts "Invalid input. Please enter a valid location (ie. 0,0)"
+      user_input = gets.chomp.split(",")
+    end
+    convert_input(user_input)
+  end
+
+  def convert_input(input_arr)
+    input_arr.map { |el| el.to_i }
+  end
+
+  def valid_input?(input_arr)
+    digits = %w[0 1 2 3 4 5 6 7 8 9]
+    if input_arr.length == 2 && input_arr.all? { |el| digits.include?(el) }
       true
     else
       false
     end
   end
+end
 
-  def end_message
-    if @game_board.counts[:ships] == 0
-      puts "Congrats, you win!"
-    else
-      puts "You lose :("
+class ComputerPlayer
+  attr_reader :name
+  attr_accessor :board
+
+  def initialize(name, board_size)
+    @board = Board.new(board_size)
+    @board_size = board_size
+    @name = name
+    @guesses = Set.new
+  end
+
+  def get_input
+    random_pick = [Random.new.rand(@board_size), Random.new.rand(@board_size)]
+    while @guesses.include?(random_pick)
+      random_pick = [Random.new.rand(@board_size), Random.new.rand(@board_size)]
     end
+    puts "random pick: #{random_pick}"
+    @guesses << random_pick
+    random_pick
   end
 end
 
-class HumanPlayer
-  #
-  # def initialize
-  # end
-
-  def get_user_input
-    puts "Where would you like to fire?"
-    user_input = gets.chomp.split(",")
-    user_input = user_input.map { |el| el.to_i }
-    until user_input.length == 2 && user_input.all? { |el| el.class == Fixnum }
-      puts "Invalid input. Please enter a valid location (ie. 0,0)"
-      user_input = gets.chomp.split(",")
-      user_input = user_input.map { |el| el.to_i }
-    end
-    user_input
-  end
-
-  def check_input(input)
-    if input == "quit"
-    end
-  end
+if __FILE__ == $PROGRAM_NAME
+  Game.new.play
 end
